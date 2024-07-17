d8a37b12ae52509f6a57907c786fc4ca8798d2b8
kaushal Rao <kaushald.rao14@gmail.com>
2024-07-17 10:23:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Wel Come</h1>
+
+      {
+      /*
+        ** Open Terminal And Run 'npm run dev' **
+        ----- Please Note that you are in currunt directory ------
+        react-portfolio-biogi // currunt directory
+      */
+      
+      }
+    </>
+  )
+}
+
+export default App
